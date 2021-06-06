c460e26b9d6d250cb13ab029f7a755a9d21fdfd3
Gaurav Mahadik <gaurav1896.gm@gmail.com>
2021-06-05 18:57:05 -0700

    Adding Form and Submit Events

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,7 +1,48 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title,setTitle] = useState('');
+    const [body,setBody] = useState('');
+    const [author,setAuthor] = useState('Gaurav');
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const blog = { title,body,author }
+        console.log(blog);
+    }
+
     return ( 
         <div className="create">
             <h2> Add a new blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog Title: </label>
+                <input
+                    type="text"
+                    required
+                    value = {title}
+                    onChange = { (e) => setTitle(e.target.value)}
+                >
+                </input>
+                <label>Blog Body: </label>
+                <textarea
+                    required
+                    value = {body}
+                    onChange = { (e) => setBody(e.target.value)}
+                >
+                </textarea>
+                <label>Blog Author: </label>
+                <select
+                    value = {author}
+                    onChange = { (e) => setAuthor(e.target.value)}
+                >
+                    <option value="Gaurav">Gaurav</option>
+                    <option value="Flash">Flash</option>
+                </select>
+                <button>Add Blog</button>
+                <p> { title }</p>
+                <p> { body }</p>
+                <p> { author }</p>
+            </form>
         </div>
      );
 }
